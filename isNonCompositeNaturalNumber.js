c30550b60eb6c2159b352c693d708d34e5a4ec7e
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-07-23 20:23:53 +0200

    primes to non-composite, range() to stepper() migrations

---
+++ isNonCompositeNaturalNumber.js
@@ -0,0 +1,39 @@
+let limit=20
+print('non-composite numbers')
+for(let n of nonCompositeNaturalNumbersGenerator(0,limit)){
+  print(n)
+}
+
+function isNonCompositeNaturalNumber(n){
+  let prime=true
+  for(let i of stepper(0,n)){
+    if(n%i==0 && i!=1){
+      prime=false
+      break
+    }
+  }
+  return prime
+}
+
+function* nonCompositeNaturalNumbersGenerator(start,limit){
+  for(let n of stepper(start,limit))
+    if(isNonCompositeNaturalNumber(n))
+      yield n
+}
+
+function* stepper(start=0, limit=NaN, step=1){
+  function continueIf(i){
+    return i<=limit || isNaN(limit)
+  }
+  function validLimit(){
+    return limit>=start || isNaN(limit)
+  }
+  if(!validLimit()) return
+  
+  for(let i=start; continueIf(i); i+=step)
+    yield i
+}
+
+function print(...args){
+  console.log(...args)
+}
